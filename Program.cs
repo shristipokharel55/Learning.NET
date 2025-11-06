d0ef5e34f9a085c5400d46f63401ca2e9174d5f8
Shristi Pokharel <shristipokharel889@gmail.com>
2025-11-06 16:10:00 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Week2Workshop
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //task 1
+            String userName = "Shristi";
+            int luckyNumber = 5;
+
+            Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+
+            //task 2 (1st process)
+            const double PI = 3.14;
+            double radius = 7.0;
+            double area = PI * radius * radius;
+            double perimeter = 2 * PI * radius;
+            Console.WriteLine($"For a circle with radius {radius}:");
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
+        }
+    }   
+}
